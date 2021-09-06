20b82facfe7d3f244b37ab4dc87088b28c8a99a6
Ömer Hodo <omerhodo@gmail.com>
2021-09-06 21:13:19 +0300

    two tabs added

--- App.js
+++ App.js
@@ -1,53 +1,65 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { Text } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import {Ionicons} from '@expo/vector-icons'
-import Chat from "./screens/Chats"
+import Chats from './screens/Chats'
+import Settings from "./screens/Settings";
+import {Ionicons} from "@expo/vector-icons"
 
-
-const Home = ()=>{
+const ChatsStack = createNativeStackNavigator()
+const ChatsScreen = () => {
   return(
-    <Text>Hi, Home screen</Text>
+    <ChatsStack.Navigator>
+      <ChatsStack.Screen name="Chats" component={Chats} />
+    </ChatsStack.Navigator>
   )
 }
 
-const Settings = ()=>{
+const SettingsStack = createNativeStackNavigator()
+const SettingsScreen = ()=>{
   return(
-    <Text>Hi, Settings screen</Text>
+    <SettingsStack.Navigator>
+      <SettingsStack.Screen name="Settings" component={Settings}/>
+    </SettingsStack.Navigator>
   )
 }
 
 const Tabs = createBottomTabNavigator()
+const TabsScreen = () => {
+  return(
+  <Tabs.Navigator
+  screenOptions={({ route }) => ({
+    tabBarIcon: ({ focused, color, size }) => {
+      let iconName;
 
-const App=()=>{
+      if (route.name === 'Chats') {
+        iconName = focused
+          ? 'chatbubbles'
+          : 'chatbubbles-outline';
+      } else if (route.name === 'Settings') {
+        iconName = focused ? 'settings' : 'settings-outline';
+      }
+
+      // You can return any component that you like here!
+      return <Ionicons name={iconName} size={size} color={color} />;
+    }
+  })}
+  >
+    <Tabs.Screen name="Chats" component={Chats}/>
+    <Tabs.Screen name="Settings" component={Settings}/>
+  </Tabs.Navigator>
+  )
+}
+
+const MainStack = createNativeStackNavigator();
+const App = ()=>{
   return (
     <NavigationContainer>
-      <Tabs.Navigator
-       screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName;
-
-          if (route.name === 'Home') {
-            iconName = focused
-              ? 'chatbubbles'
-              : 'chatbubbles-outline';
-          } else if (route.name === 'Settings') {
-            iconName = focused ? 'settings' : 'settings-outline';
-          }
-
-          // You can return any component that you like here!
-          return <Ionicons name={iconName} size={size} color={color} />;
-        },
-        tabBarActiveTintColor: 'tomato',
-        tabBarInactiveTintColor: 'gray',
-      })}
-      >
-        <Tabs.Screen name="Home" component={Home}/>
-        <Tabs.Screen name="Settings" component={Settings}/>
-      </Tabs.Navigator>
+      <MainStack.Navigator headerMode="none">
+        <MainStack.Screen name="Tabs" component={TabsScreen} />
+      </MainStack.Navigator>
     </NavigationContainer>
   )
 }
 
-export default App;
+export default App
---
+++ screens/Settings.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { Text } from 'react-native'
+
+const Settings =()=> {
+    return (
+        <Text>Settings</Text>        
+    )
+}
+
+export default Settings
