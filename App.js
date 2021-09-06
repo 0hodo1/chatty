7e61d076b857dbd72f3c8f63e0ac48375074fb6c
Ömer Hodo <omerhodo@gmail.com>
2021-09-06 20:13:03 +0300

    navigation test added

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { Text } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {Ionicons} from '@expo/vector-icons'
 import Chat from "./screens/Chats"
 
 
@@ -22,7 +23,26 @@ const Tabs = createBottomTabNavigator()
 const App=()=>{
   return (
     <NavigationContainer>
-      <Tabs.Navigator>
+      <Tabs.Navigator
+       screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = focused
+              ? 'chatbubbles'
+              : 'chatbubbles-outline';
+          } else if (route.name === 'Settings') {
+            iconName = focused ? 'settings' : 'settings-outline';
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'gray',
+      })}
+      >
         <Tabs.Screen name="Home" component={Home}/>
         <Tabs.Screen name="Settings" component={Settings}/>
       </Tabs.Navigator>
